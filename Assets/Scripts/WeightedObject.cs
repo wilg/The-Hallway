fa2a081e00dd2f2831b55d1d7eaee8803482c318
Wil Gieseler <supapuerco@gmail.com>
2011-10-09 18:13:46 -0700

    WeightedObject now reports whether it is grounded

--- Assets/Scripts/WeightedObject.cs
+++ Assets/Scripts/WeightedObject.cs
@@ -15,5 +15,25 @@ public class WeightedObject : MonoBehaviour {
     public Bounds PhysicalBounds() {
         return collider.bounds;
     }
+    
+    bool _isGrounded;
+    
+    public bool IsGrounded() {
+        return _isGrounded;
+    }
+    
+    void OnCollisionEnter(Collision collision) {
+        // is on layer for Platform
+        if (collision.gameObject && collision.gameObject.layer == 8) {
+            _isGrounded = true;
+        }
+    }
+    
+    void OnCollisionExit(Collision collision) {
+        // is on layer for Platform
+        if (collision.gameObject && collision.gameObject.layer == 8) {
+            _isGrounded = false;
+        }
+    }
 
 }
